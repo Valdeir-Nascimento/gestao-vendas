600543df5d25b77de2f78688abd18e229170c485
Valdeir Nascimento <valdeircn11@gmail.com>
2021-12-26 18:53:36 -0300

    Documentação da API com Swagger 2

--- src/main/java/br/com/gvendas/controller/CategoriaController.java
+++ src/main/java/br/com/gvendas/controller/CategoriaController.java
@@ -4,6 +4,8 @@ import br.com.gvendas.dto.CategoriaDTO;
 import br.com.gvendas.dto.converter.CategoriaDTOConverter;
 import br.com.gvendas.model.Categoria;
 import br.com.gvendas.service.CategoriaService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 import java.util.List;
 
+@Api(tags = "Categoria")
 @RestController
 @RequestMapping("/categorias")
 public class CategoriaController {
@@ -21,18 +24,21 @@ public class CategoriaController {
     @Autowired
     private CategoriaDTOConverter categoriaDTOConverter;
 
+    @ApiOperation(value = "Listar")
     @GetMapping
     public ResponseEntity<List<CategoriaDTO>> listar() {
         List<Categoria> categorias = categoriaService.listar();
         return ResponseEntity.ok().body(categoriaDTOConverter.toList(categorias));
     }
 
+    @ApiOperation(value = "Buscar por ID")
     @GetMapping("/{idCategoria}")
     public ResponseEntity<CategoriaDTO> buscarPorId(@PathVariable Long idCategoria) {
         var categoria = categoriaService.buscarPorId(idCategoria);
         return ResponseEntity.ok().body(categoriaDTOConverter.to(categoria));
     }
 
+    @ApiOperation(value = "Criar nova categoria")
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public ResponseEntity<CategoriaDTO> salvar(@Valid @RequestBody CategoriaDTO categoriaDTO) {
@@ -41,6 +47,7 @@ public class CategoriaController {
         return ResponseEntity.ok().body(categoriaDTOConverter.to(categoria));
     }
 
+    @ApiOperation(value = "Atualizar")
     @PutMapping("/{idCategoria}")
     public ResponseEntity<CategoriaDTO> atualizar(@PathVariable Long idCategoria, @Valid @RequestBody CategoriaDTO categoriaDTO) {
         Categoria categoriaAtual = categoriaService.buscarPorId(idCategoria);
